777bc13988276387a8e8448e8f41a8802c17f862
Cici <45863430+Matrix-Cc@users.noreply.github.com>
2020-08-03 10:50:32 +0800

    Add files via upload

--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -1,13 +1,49 @@
 // pages/cart/cart.js
+//获取应用实例
+const app = getApp()
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-
+		num:1,
+		minusStatus:'disable'
 	},
-
+//事件处理函数
+  /*点击减号*/
+  bindMinus: function() {
+    var num = this.data.num;
+    if (num>1) {
+      num--;
+    }
+    var minusStatus = num>1 ? 'normal':'disable';
+    this.setData({
+      num:num,
+      minusStatus:minusStatus
+    })
+  },
+  /*点击加号*/
+  bindPlus: function() {
+    var num = this.data.num;
+    num++;
+    var minusStatus = num > 1 ? 'normal' : 'disable';
+    this.setData({
+      num:num,
+      minusStatus: minusStatus
+    })
+  },
+  /*输入框事件*/
+  bindManual: function(e) {
+    var num = e.detail.value;
+    var minusStatus = num > 1 ? 'normal' : 'disable';
+    this.setData({
+      num:num,
+      minusStatus: minusStatus
+    })
+	},
+	
+	
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,24 +1,29 @@
 Page({
 
   data: {
-    
+  
   },
-	
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function (options) {
     
   },
+
+
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   onUnload: function () {
-
+    
   },
 
   onShareAppMessage: function () {
-      
+    
   }
 })
