d13527c47a009ae5eff9c3902157dc6697766d80
Mountain-Z <you@example.com>
2020-08-23 14:10:08 +0800

    profile init

--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -1,66 +1,86 @@
-// pages/profile/profile.js
+//获取应用实例
+const app = getApp();
+
 Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+   /*  orderItems: [
+      {        
+        typeId: 0,        
+        name: '待付款',        
+        url: '../detail/detail',        
+        imageurl: '../../images/待支付.png',
+      },
+      {        
+        typeId: 1,        
+        name: '待发货',        
+        url: '',        
+        imageurl: '../../images/待发货.png',
+      },
+      {        
+        typeId: 2,        
+        name: '待收货',        
+        url: '',        
+        imageurl: '../../images/待收货.png'
+      },
+      {        
+        typeId: 3,        
+        name: '已完成',        
+        url: '',        
+        imageurl: '../../images/已完成.png'
+      }
+    ], */
+    menuitems: [
+      { text: '查看订单', url: '', imgurl:'../../images/订单.png'  },
+      { text: '收货地址', url: '../addressList/addressList', imgurl:'../../images/收货地址.png'  },
+      { text: '联系客服', url: '../index/index', imgurl:'../../images/联系客服.png' }
+    ]
+  },
 
-	/**
-	 * 页面的初始数据
-	 */
-	data: {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    if (app.globalData.userInfo) {
+      that.setUserInfo(app.globalData.userInfo);
+    } else if (that.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        that.setUserInfo(res.userInfo);
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          that.setUserInfo(res.userInfo);
+        }
+      })
+    }
+  },
 
-	},
+  getUserInfo: function (e) {
+    this.setUserInfo(e.detail.userInfo);
+  },
 
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad: function (options) {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
-	}
+  setUserInfo: function (userInfo) {
+    if (userInfo != null) {
+      app.globalData.userInfo = userInfo
+      this.setData({
+        userInfo: userInfo,
+        hasUserInfo: true
+      })
+    }
+  }/* ,
+  toOrder: function () {
+    wx.navigateTo({      url: 'this.orderItems.url'
+    })
+  }
+ */
 })
